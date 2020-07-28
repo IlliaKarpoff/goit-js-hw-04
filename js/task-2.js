3ada10fd17ef7ffdfd6c4e7d226a3ecd97fb5577
Illia Karpoff <hawaii2001@ukr.net>
2020-07-29 00:15:32 +0300

    tasks 2 aproved

--- js/task-2.js
+++ js/task-2.js
@@ -6,27 +6,27 @@ const inventory = {
   add(itemName) {
     console.log(`Adding ${itemName} to inventory`);
 
-    this.items.push(itemName);
+    inventory.items.push(itemName);
   },
   remove(itemName) {
     console.log(`Removing ${itemName} from inventory`);
 
-    this.items = this.items.filter(item => item !== itemName);
+    inventory.items = inventory.items.filter(item => item !== itemName);
   },
-  invokeInventoryAction(itemName, action) {
-    console.log(`Invoking action on ${itemName}`);
-    action(itemName);
-    }
 };
 
+const invokeInventoryAction = function (itemName, action) {
+  console.log(`Invoking action on ${itemName}`);
+  action(itemName);
+};
 
-inventory.invokeInventoryAction('Medkit', inventory.add);
+invokeInventoryAction('Medkit', inventory.add);
 // Invoking action on Medkit
 // Adding Medkit to inventory
 
 console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
 
-inventory.invokeInventoryAction('Gas mask', inventory.remove);
+invokeInventoryAction('Gas mask', inventory.remove);
 // Invoking action on Gas mask
 // Removing Gas mask from inventory
 
