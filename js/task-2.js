dddb0cc5af75b1695979f01f60f17386f34d7875
Illia Karpoff <hawaii2001@ukr.net>
2020-07-28 23:58:27 +0300

    tasks 1-2

---
+++ js/task-2.js
@@ -0,0 +1,33 @@
+// Завдання 2
+// Виправ помилки, які будуть в консолі, щоб скрипт запрацював.
+
+const inventory = {
+  items: ['Knife', 'Gas mask'],
+  add(itemName) {
+    console.log(`Adding ${itemName} to inventory`);
+
+    this.items.push(itemName);
+  },
+  remove(itemName) {
+    console.log(`Removing ${itemName} from inventory`);
+
+    this.items = this.items.filter(item => item !== itemName);
+  },
+  invokeInventoryAction(itemName, action) {
+    console.log(`Invoking action on ${itemName}`);
+    action(itemName);
+    }
+};
+
+
+inventory.invokeInventoryAction('Medkit', inventory.add);
+// Invoking action on Medkit
+// Adding Medkit to inventory
+
+console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
+
+inventory.invokeInventoryAction('Gas mask', inventory.remove);
+// Invoking action on Gas mask
+// Removing Gas mask from inventory
+
+console.log(inventory.items); // ['Knife', 'Medkit']
